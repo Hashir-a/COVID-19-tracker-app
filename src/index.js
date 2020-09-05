25d90f901808485fb7a54b6e769eb0584a4f1200
Muhammad Hashir Mohsineen <60481890+Hashir-a@users.noreply.github.com>
2020-09-05 20:26:20 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'fontsource-roboto';
-//import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
 ReactDOM.render(
  <App/>,
   document.getElementById('root')
